be71c51fa73afe84cff5188e21d998c996978333
Bibek Singh <bbek1717ingh@gmail.com>
2026-06-25 14:14:16 +0545

    connected to database

--- server/server.js
+++ server/server.js
@@ -1,6 +1,13 @@
+import {setServers} from "node:dns/promises"
+setServers(["1.1.1.1", "8.8.8.8"]); 
+
+import 'dotenv/config'
+
 import app from "./src/app.js";
+import connectToDb from './src/config/database.js';
 
 
+connectToDb()
 
 app.listen(3000, ()=> {
     console.log('server is running at port 3000')
