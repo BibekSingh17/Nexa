a1bcbb0f5c964e51ceb43fe1282caa23a429fd2b
Bibek Singh <bbek1717ingh@gmail.com>
2026-07-14 10:47:44 +0545

    cors

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,7 @@ import express from 'express'
 import authRouter from './route/auth.route.js'
 import cookieParser from 'cookie-parser'
 import morgan from 'morgan'
+import cors from 'cors';
 
 const app = express()
 
@@ -9,7 +10,10 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser())
 app.use(morgan('dev'))
-
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}))
 
 app.use('/api/auth', authRouter)
 
